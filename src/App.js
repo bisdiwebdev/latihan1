233fafd7dc64df097c53e714e79a84c07a1690b8
bisdiwebdev <81268563+bisdiwebdev@users.noreply.github.com>
2024-05-26 21:09:56 +0700

    Update App.js, add a new paragraph.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>Hi, I am learning web development with React.</p>
+        <p>I am still trying to understand how to use props.</p>
       </header>
     </div>
   );
